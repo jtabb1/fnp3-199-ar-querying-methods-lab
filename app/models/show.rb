dc7483f232fadf0b45d4b73c5847f43bd0a58848
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-25 22:27:05 -0400

    Note and get alternate soltuion tested git push

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,28 +4,46 @@ class Show < ActiveRecord::Base
     self.maximum("rating")
   end
 
-  def self.most_popular_show
+  def self.most_popular_show_01
     self.find_by(rating: self.highest_rating)
+  end
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
     self.minimum("rating")
   end
 
-  def self.least_popular_show
+  def self.least_popular_show_01
     self.find_by(rating: self.lowest_rating)
+  end
+
+  # this method from the official solution also works:
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
   end
 
   def self.ratings_sum
     self.sum("rating")
   end
 
-  def self.popular_shows
+  def self.popular_shows_01
     self.where("rating > 5")
   end
 
-  def self.shows_by_alphabetical_order
-    self.all.order("name")
+  # this method from the official solution also works:
+  def self.popular_shows
+    self.where("rating >= ?", 5)
   end
 
+  def self.shows_by_alphabetical_order_01
+    self.all.order("name")
+  end
+  
+  # this method from the official solution also works:
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
 end
